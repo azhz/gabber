48276e348e64b4d5e905a1fe049028f68a245225
Ajinkya H <ajinkya233@gmail.com>
2018-01-14 11:29:51 +0530

    Changed directory structure and some code changes
    - Moved index.html to /public
    - Update page title

--- index.js
+++ index.js
@@ -3,25 +3,24 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-	//res.send('<h1> Hello, World! </h1>');
-	res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 var total_users = 0;
 
 io.on('connection', function(socket) {
-	
-	total_users++;
-	io.emit('user connected', total_users);
-	
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
-	});
-	
-	socket.on('disconnect', function () {
-		total_users--;
-		io.emit('user disconnected', total_users);
-	});
+
+    total_users++;
+    io.emit('user connected', total_users);
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function () {
+        total_users--;
+        io.emit('user disconnected', total_users);
+    });
 });
 
 http.listen((process.env.PORT || 3000), function() {
